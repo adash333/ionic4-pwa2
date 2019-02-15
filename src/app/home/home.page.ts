819d7dd97972843f550c45b3e693481c9cf2e7b2
adash333 <adash333@hotmail.com>
2019-02-16 01:22:37 +0900

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  showBtn: boolean = false;
+  deferredPrompt;
+ 
+  constructor() {
+  }
+ 
+  ngOnInit(){
+    this.showBtn = true;
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later on the button event.
+      this.deferredPrompt = e;
+ 
+    // Update UI by showing a button to notify the user they can add to home screen
+      this.showBtn = true;
+    });
+ 
+    //button click event to show the promt
+ 
+    window.addEventListener('appinstalled', (event) => {
+     alert('installed');
+    });
+ 
+ 
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      alert('display-mode is standalone');
+    }
+  }
+ 
+  add_to_home(e){
+    debugger
+    // hide our user interface that shows our button
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          alert('User accepted the prompt');
+        } else {
+          alert('User dismissed the prompt');
+        }
+        this.deferredPrompt = null;
+      });
+  };
 }
